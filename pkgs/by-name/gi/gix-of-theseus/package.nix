68003f57b05866d97e58cd7ea4be8f2173810b19
Daniel Woffinden <daw@hey.com>
2026-09-01 23:20:42 +0100

    gix-of-theseus: init
    https://amedee.me/introducing-gix-of-theseus

---
+++ pkgs/by-name/gi/gix-of-theseus/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "gix-of-theseus";
+  version = "0-unstable-2025-11-04";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "amedeedaboville";
+    repo = "gix-of-theseus";
+    rev = "33ae4919c326cfcf6ab50fde5eaac425145b905c";
+    hash = "sha256-79JfmLlmUftdxw7NI/VWuDBFqaxLaG6eHIPuYip4hfg=";
+  };
+
+  cargoHash = "sha256-GLfHQeJGAbFQVTek1hBZ8U2omyiuty2nnnHdbdlG0/4=";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "A Rust rewrite of git-of-theseus";
+    homepage = "https://github.com/amedeedaboville/gix-of-theseus";
+    changelog = "https://github.com/amedeedaboville/gix-of-theseus/blob/${finalAttrs.src.rev}/CHANGELOG.md";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "gix-of-theseus";
+  };
+})
